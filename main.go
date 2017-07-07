118696e201c899a6a71ff6751764783ab747ef0b
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2017-07-07 21:51:19 +0200

    Simple working example using public-folder as fs and memls as lock system. Tested with osx

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/net/webdav"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//fs := webdav.NewMemFS()
+	cwd, _ := os.Getwd()
+	fs := webdav.Dir(filepath.Join(cwd, "public"))
+	ls := webdav.NewMemLS()
+	l := log.New(os.Stderr, "[webdav] ", log.Lshortfile|log.LstdFlags|log.LUTC)
+	h := &webdav.Handler{
+		Prefix:     "/",
+		FileSystem: fs,
+		LockSystem: ls,
+		Logger: func(r *http.Request, err error) {
+			if err != nil {
+				l.Printf("Error=%q from req=%#v\n", err, *r)
+			} else {
+				l.Printf("Request: %d %s %s\n", r.ContentLength, r.Method, r.URL)
+			}
+		},
+	}
+	log.Fatalln(http.ListenAndServe(":8080", h))
 }
